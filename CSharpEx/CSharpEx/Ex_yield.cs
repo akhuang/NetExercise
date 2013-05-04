4902095a67acdb3c09a19964bab76e2776124b61
fy24h <fy24h.huang@gmail.com>
2013-05-04 18:10:29 +0800

    add C# exercises

---
+++ CSharpEx/CSharpEx/Ex_yield.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpEx
+{
+    public class Ex_yield
+    {
+
+    }
+
+    public class DaysOfTheWeek : IEnumerable
+    {
+        string[] days = { "Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat" };
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var item in days)
+            {
+                yield return item;
+            }
+        }
+
+
+    }
+}
---
+++ CSharpEx/CSharpEx/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DaysOfTheWeek week = new DaysOfTheWeek();
+            foreach (var item in week)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
