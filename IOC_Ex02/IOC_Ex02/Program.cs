5373606ca4a24ccb90434dd48672ccc958b05565
phoenix <fy24h.huang@gmail.com>
2013-12-04 09:17:04 +0800

    ioc ex

---
+++ IOC_Ex02/IOC_Ex02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IOC_Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Monster m1 = new Monster("小怪", 30);
+            Monster m3 = new Monster("中怪", 50);
+            Monster m4 = new Monster("Boss", 1000);
+
+            Roles role = new Roles();
+            role.Weapon = WeaponType.WoodenSword;
+            RolesServices rs = new RolesServices(role);
+            rs.AttackMonster(m1);
+            rs.AttackMonster(m1);
+            rs.AttackMonster(m1);
+
+            role.Weapon = WeaponType.IconSword;
+
+            rs.AttackMonster(m3);
+            rs.AttackMonster(m3);
+            rs.AttackMonster(m3);
+
+            role.Weapon = WeaponType.MagicSword;
+            rs.AttackMonster(m4);
+            rs.AttackMonster(m4);
+            rs.AttackMonster(m4);
+            rs.AttackMonster(m4);
+            rs.AttackMonster(m4);
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ IOC_Ex02/IOC_Ex02/Roles.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IOC_Ex02
+{
+    public class Roles
+    {
+        public string Name { get; set; }
+        public WeaponType Weapon { get; set; }
+    }
+
+    public class Monster
+    {
+        public Monster(string name, int hp)
+        {
+            Name = name;
+            HP = hp;
+        }
+        public string Name { get; set; }
+        public int HP { get; set; }
+    }
+
+    public enum WeaponType
+    {
+        WoodenSword = 1,
+        IconSword = 2,
+        MagicSword = 3
+    }
+}
