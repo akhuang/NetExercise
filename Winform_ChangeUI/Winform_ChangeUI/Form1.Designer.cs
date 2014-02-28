984acbc70341596c3051b6f2d867a7cfce2ee2e5
phoenix <fy24h.huang@gmail.com>
2014-02-28 15:46:37 +0800

    add Winform_ChangeUI

---
+++ Winform_ChangeUI/Winform_ChangeUI/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Winform_ChangeUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listLog = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listLog
+            // 
+            this.listLog.FormattingEnabled = true;
+            this.listLog.Location = new System.Drawing.Point(-1, -2);
+            this.listLog.Name = "listLog";
+            this.listLog.Size = new System.Drawing.Size(812, 407);
+            this.listLog.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 521);
+            this.Controls.Add(this.listLog);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listLog;
+    }
+}
+
---
+++ Winform_ChangeUI/Winform_ChangeUI/Utils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Winform_ChangeUI
+{
+    public class Utils
+    {
+
+    }
+}
